e0b544a63d5af120661cc69f1783e92e2b9dd61c
Alex <37036976+lexient@users.noreply.github.com>
2026-04-16 21:48:23 +1000

    scrape and README

---
+++ README.md
@@ -0,0 +1,23 @@
+# seek-scrape
+
+Scrapes jobs from seek.com.au.
+
+The search API (/api/jobsearch/v5/search) is public, however the API for fetching the content of the job listings is not.
+
+This script obtains job IDs from the search API and downloads the HTML of the job pages themselves.
+
+Cloudflare detection is bypassed using a browser's TLS/JA3 fingerprint and HTTP/2 behaviour using [curl_cffi](https://github.com/lexiforest/curl_cffi).
+
+## Usage
+
+```bash
+git clone https://github.com/lexient/seek-scrape
+cd seek-scrape
+
+# set up venv
+python -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+python scrape.py
+```
---
+++ scrape.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from curl_cffi import requests
+import os
+import sys
+import time
+
+
+# https://curl-cffi.readthedocs.io/en/latest/quick_start.html#requests-like
+session = requests.Session()
+
+
+limit = 5
+page = 1
+jobs_saved = 0
+
+while jobs_saved < limit:
+    # get search results for this page
+    search_url = "https://www.seek.com.au/api/jobsearch/v5/search?classification=6281&page=" + str(page)
+    search_response = session.get(search_url, impersonate="chrome100")
+    search_results = search_response.json()
+    # print(search_results)
+    jobs = search_results["data"]
+
+    for job in jobs:
+        job_id = job["id"]
+        job_title = job["title"]
+        
+        job_url = "https://www.seek.com.au/job/" + str(job_id)
+        job_response = session.get(job_url, impersonate="chrome100")
+        job_html = job_response.text
+        
+        filename = "jobs/" + str(job_id) + ".html"
+        file = open(filename, "w")
+        file.write(job_html)
+        file.close()
+        
+        print(jobs_saved + 1, job_id, "-", job_title)
+        jobs_saved = jobs_saved + 1
+        
+        if jobs_saved >= limit:
+            break
+    
+    page = page + 1
